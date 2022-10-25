e7e3e0fbb8c9622a57b64364eb3a1643cda92430
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-10-24 22:39:19 -0300

    added show json

--- app/controllers/api/v1/releases_controller.rb
+++ app/controllers/api/v1/releases_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::ReleasesController < Api::V1::BaseController
+  before_action :set_release, only: [ :show ]
   def index
     @releases = Release.all
     render json: @releases
   end
+
+  def show
+  end
+
+  private
+
+  def set_release
+    @release = Release.find(params[:id])
+    render json: @release
+  end
+
 end
---
+++ app/views/api/v1/releases/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @release, :id, :name
