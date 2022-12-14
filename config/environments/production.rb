c1828c0f9420aedd98f2ecd131d978570c1917c7
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-12-14 18:43:07 -0300

    added host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "gpgc-api-production.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
