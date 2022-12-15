fb09e5a38785b3b565236ab078d7844f557b3632
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-12-15 19:37:04 -0300

    removed remix seeds and added featuring seeds

--- db/seeds_files/featuring_seeds.rb
+++ db/seeds_files/featuring_seeds.rb
@@ -19,6 +19,26 @@ Featuring.find_or_create_by!(
 )
 
 Featuring.find_or_create_by!(
+  featuring_name: "Stop Waiting (Versão Guerra)",
+  credit: ["Remixer"],
+  release_date: Date.new(2012, 2, 19),
+  is_album:  false,
+  label: "Balaclava Records",
+  artist: "Single Parents",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
+  featuring_name: "Full Of Life (R.B.G)",
+  credit: ["Remixer"],
+  release_date: Date.new(2012, 9, 12),
+  is_album:  false,
+  label: "Trama",
+  artist: "Holger",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
   featuring_name: "Speedrun",
   credit: ["Mixing engineer"],
   release_date: Date.new(2018, 11,  7),
@@ -37,12 +57,43 @@ Featuring.find_or_create_by!(
 )
 
 Featuring.find_or_create_by!(
+  featuring_name: "Proibidão (Repetentes 2008 Remix)",
+  credit: ["Remixer"],
+  release_date: Date.new(2019, 4, 23),
+  is_album:  false,
+  label: "Massa Records",
+  artist: "Renato Cohen & Rocco Sledge",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
+  featuring_name: "As Velas da Cantareira (DJ Guerrinha Mix)",
+  credit: ["Remixer"],
+  release_date: Date.new(2019, 5, 1),
+  is_album:  false,
+  label: "Alliance Upholstery",
+  artist: "Séculos Apaixonados",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
   featuring_name: "2",
   credit: ["Musician"],
   release_date: Date.new(2019, 6,  7),
   is_album:  true,
   label: "PAN",
   artist: "Lifted"
+)
+
+
+Featuring.find_or_create_by!(
+  featuring_name: "Sexy Savannah (Repetentes 2008 Remix)",
+  credit: ["Remixer"],
+  release_date: Date.new(2019, 9, 20),
+  is_album:  false,
+  label: "Risco",
+  artist: "Jonas Sá",
+  info: ""
 )
 
 
@@ -63,11 +114,51 @@ Featuring.find_or_create_by!(
 )
 
 Featuring.find_or_create_by!(
+  featuring_name: "BFF (D.I.R. 119 BPM Dub)",
+  credit: ["Remixer"],
+  release_date: Date.new(2020, 5, 22),
+  is_album:  false,
+  label: "Gop Tun",
+  artist: "TYV",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
+  featuring_name: "Flying Nine (Dara) (Repetentes 2008 Remix)",
+  credit: ["Remixer"],
+  release_date: Date.new(2020, 7, 2),
+  is_album:  false,
+  label: "Confuso Editions",
+  artist: "John Zieman",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
   featuring_name: "Circuit Break",
   credit: ["Mixing engineer"],
   release_date: Date.new(2020, 11, 27),
   is_album:  false,
   artist: "Noporn"
+)
+
+Featuring.find_or_create_by!(
+  featuring_name: "Monsters In The Dark (Repetentes 2008 Remix)",
+  credit: ["Remixer"],
+  release_date: Date.new(2020, 11, 22),
+  is_album:  false,
+  label: "Karuana",
+  artist: "Day Out Of Time",
+  info: ""
+)
+
+Featuring.find_or_create_by!(
+  featuring_name: "Deixa Tudo Fluir (Guerrinha's Deixa o Orgão Solar Mix)",
+  credit: ["Remixer"],
+  release_date: Date.new(2021, 1, 27),
+  is_album:  false,
+  label: "Let's Play House",
+  artist: "Saidêra",
+  info: ""
 )
 
 Featuring.find_or_create_by!(
