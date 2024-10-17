570676b85efc6861cfcfa6cd74a4f28880ffd26b
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-17 17:36:09 -0300

    created up script

---
+++ devops/compose/up.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# set -e
+
+#. devops/compose/.env
+
+#COMPOSE_FILE=$COMPOSE_FILE \
+#ARG_USER_UID=$ARG_USER_UID \
+#ARG_USER_GID=$ARG_USER_GID \
+#DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM \
+docker compose up -d
