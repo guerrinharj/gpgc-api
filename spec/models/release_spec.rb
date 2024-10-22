45154a1700f4e41bcac6e8ef528e4f0e07b38d49
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-22 20:48:29 -0300

    created basic testing for release

---
+++ spec/models/release_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Release, type: :model do
+    let(:artist) { create(:artist) } 
+
+    describe 'associations' do
+        it 'belongs to an artist' do
+        release = Release.new(artist: artist)
+        expect(release.artist).to eq(artist)
+        end
+    end
+
+    describe 'validations' do
+        it 'is valid with valid attributes' do
+        release = build(:release, artist: artist)
+        expect(release).to be_valid
+        end
+
+        it 'is invalid without a name' do
+        release = build(:release, name: nil, artist: artist)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid without an artist' do
+        release = build(:release, artist: nil)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid without a slug' do
+        release = build(:release, slug: nil, artist: artist)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid with a duplicate slug' do
+        create(:release, slug: 'album_name')
+        release = build(:release, slug: 'album_name', artist: artist)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid without credits' do
+        release = build(:release, credits: nil, artist: artist)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid without links' do
+        release = build(:release, links: nil, artist: artist)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid with incorrect credits format' do
+        release = build(:release, credits: 'Invalid Format', artist: artist)
+        expect(release).not_to be_valid
+        end
+
+        it 'is invalid with incorrect links format' do
+        release = build(:release, links: 'Invalid Format', artist: artist)
+        expect(release).not_to be_valid
+        end
+    end
+end
