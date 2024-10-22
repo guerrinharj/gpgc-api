09b083d9a0b259a91c4fd7bc173688d02e49400a
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-22 19:35:35 -0300

    new shell commands

--- devops/rails/restart.sh
+++ devops/rails/restart.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
 chmod +x ./devops/rails/restart.sh
+docker compose run web bundle install
 docker compose run web rails db:drop db:create db:migrate db:seed
---
+++ devops/rails/update.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+chmod +x ./devops/rails/update.sh
+docker compose run web bundle install
+docker compose run web rails db:migrate db:seed
