263e841b2081deabe44c4316fe50a4ce545709b6
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-22 20:48:41 -0300

    changed model associates

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,10 +1,36 @@
 class Release < ApplicationRecord
-  serialize :credits
-  serialize :links
+  attribute :cover, :string, array: true, default: []
+  attribute :label, :string, array: true, default: []
+  attribute :format, :string, array: true, default: []
+  attribute :notes, :string, array: true, default: []
+  attribute :credits, :json, default: {}
+  attribute :links, :json, default: {}
+
+  validates :artist, presence: true
+  validates :artist_name, :name, :slug, presence: true
+  validates :slug, uniqueness: true
+  validates :cover, :label, :format, :notes, presence: true
+  validate :validate_credits_format
+  validate :validate_links_format
+
   belongs_to :artist
 
-def to_param
-  "#{slug}"
-end
+  def to_param
+    "#{slug}"
+  end
 
+
+  private
+
+  def validate_credits_format
+    unless credits.is_a?(Hash)
+      errors.add(:credits, 'must be a hash')
+    end
+  end
+
+  def validate_links_format
+    unless links.is_a?(Hash)
+      errors.add(:links, 'must be a hash')
+    end
+  end
 end
