0abeacad080a01e5e6f72da0765693d5b0bd3adb
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-22 21:48:30 -0300

    Edited restart shell script

--- devops/rails/restart.sh
+++ devops/rails/restart.sh
@@ -3,7 +3,10 @@
 chmod +x ./devops/rails/restart.sh
 docker compose run web bundle install
 
-docker compose run web rails db:drop db:create db:migrate db:seed RAILS_ENV=development
+docker compose run web rails db:environment:set RAILS_ENV=development
+docker compose run web rails db:drop
+docker compose run web rails db:create
+docker compose run web rails db:migrate RAILS_ENV=development
+docker compose run web rails db:seed RAILS_ENV=development
 
-docker compose run web rails db:drop db:create db:migrate RAILS_ENV=test
 docker compose run web rspec   
