d12944532c1b8765271e5819c9a08f1f11a20df4
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-22 22:07:59 -0300

    created featuring specs and changed model

---
+++ spec/factories/featurings.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+    factory :featuring do
+        featuring_name { "John Doe" }
+        is_album { false }
+        artist { "Larry Doe" }
+        label { "Label" }
+        info { "Info" }
+        release_date { "2011-01-01" }
+        credit { ["producer", "backing vocal"] }
+        url { "http://teste.com" }
+    end 
+end
