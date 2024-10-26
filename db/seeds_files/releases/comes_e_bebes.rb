e08f923dc2298d306f59e2889e753719776d99d9
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-26 03:11:44 -0300

    db fixes

--- db/seeds_files/releases/comes_e_bebes.rb
+++ db/seeds_files/releases/comes_e_bebes.rb
@@ -1,19 +1,17 @@
 p "Seeding Comes & Bebes"
 
-Release.find_or_create_by!(
-  artist: Artist.find_by(name: "Comes & Bebes"),
-  artist_name: Artist.find_by(name: "Comes & Bebes").name,
+artist = Artist.find_or_create_by!(name: "Comes & Bebes")
+
+release_choque_termico = Release.find_or_create_by!(
+  artist: artist,
+  artist_name: artist.name,
   name: "Choque Térmico",
   slug: 'choque-termico',
-  cover: ["https://i.imgur.com/dhCS3bc.jpg"],
+  cover: ["https://f4.bcbits.com/img/a3058190152_10.jpg"],
   release_type: "Single",
   release_date: Date.new(2023, 9, 2),
   format: ["Streaming"],
-  tracks: [
-    "Choque Térmico",
-    "Sucesso Sexual"
-  ],
-    credits: {
+  credits: {
     "Luisa Arraes": "vocals",
     "Arthur Braganti": "piano",
     "Gabriel Guerra": "keyboards, programming",
@@ -27,3 +25,12 @@ Release.find_or_create_by!(
     "Spotify": "https://open.spotify.com/album/5b3GIbXtPWwIp1rNqHraHZ"
   }
 )
+
+[
+  { title: "Choque Térmico", duration: "03:38", url: "https://storage.cloud.google.com/gpgc-api-bucket/RELEASED/COMES%20%26%20BEBES/Choque%20Te%CC%81rmico/Comes%20%26%20Bebes%20-%20Choque%20T%C3%A9rmico%20-%2001%20Choque%20T%C3%A9rmico.mp3" },
+  { title: "Sucesso Sexual", duration: "04:27", url: "https://storage.cloud.google.com/gpgc-api-bucket/RELEASED/COMES%20%26%20BEBES/Choque%20Te%CC%81rmico/Comes%20%26%20Bebes%20-%20Choque%20T%C3%A9rmico%20-%2002%20Sucesso%20Sexual.mp3" }
+].each do |track|
+  release_choque_termico.songs.find_or_create_by!(track.merge(artist: artist))
+end
+
+p "Seeded Choque Térmico release and songs."
--- db/seeds_files/releases/contrahouse.rb
+++ db/seeds_files/releases/contrahouse.rb
@@ -1,32 +1,37 @@
 p "Seeding Contrahouse"
 
-Release.find_or_create_by!(
-  artist: Artist.find_by(name: "Contrahouse"),
-  artist_name: Artist.find_by(name: "Contrahouse").name,
+artist = Artist.find_or_create_by!(name: "Contrahouse")
+
+release_contrahouse = Release.find_or_create_by!(
+  artist: artist,
+  artist_name: artist.name,
   name: "Contrahouse",
   slug: 'contrahouse',
   cover: ["https://f4.bcbits.com/img/a2498817730_10.jpg"],
   release_type: "Album",
   release_date: Date.new(2024, 2, 28),
   format: ["Streaming"],
-  tracks: [
-    "Big Time Sensuality 2",
-    "Jellyfish",
-    "American Bottoms",
-    "Streetcorn "
-  ],
-    credits: {
+  credits: {
     "Bruce Hornsby": "piano",
     "Gabriel Guerra": "keyboards, programming",
     "Lucas de Paiva": "keyboards, programming",
     "Jenny Konradsen": "vocals"
   },
-  notes: [
-    "Track 1 written by Arthur Braganti",
-    "Track 2 written by Léo Jaime"
+  notes: ["All tracks produced by Gabriel Guerra and Lucas de Paiva, all tracks mixed by Gabriel Guerra"
   ],
   links: {
     "Spotify": "https://open.spotify.com/album/06C0IX5juKbWePER7mNrYY",
     "Bandcamp": "https://ulyssa.bandcamp.com/album/contrahouse"
   }
 )
+
+[
+  { title: "Big Time Sensuality 2", duration: "09:55", url: "" },
+  { title: "Jellyfish", duration: "10:14", url: "" },
+  { title: "American Bottoms", duration: "07:31", url: "" },
+  { title: "Streetcorn", duration: "08:22", url: "" }
+].each do |track|
+  release_contrahouse.songs.find_or_create_by!(track.merge(artist: artist))
+end
+
+p "Seeded Contrahouse release and songs."
