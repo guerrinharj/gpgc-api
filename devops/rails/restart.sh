cd58d368d26034636f21f593fa28c7823c12d123
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-31 19:23:13 -0300

    changed to port 3000 and development mode

--- devops/rails/restart.sh
+++ devops/rails/restart.sh
@@ -5,11 +5,11 @@ bundle install
 docker compose run web bundle install
 
 docker compose run web rails db:environment:set
-docker compose run web rails db:drop RAILS_ENV=production
-docker compose run web rails db:create RAILS_ENV=production
+docker compose run web rails db:drop
+docker compose run web rails db:create
 docker compose run web rails active_storage:install
-docker compose run web rails db:migrate RAILS_ENV=production
-docker compose run web rails db:seed RAILS_ENV=production
+docker compose run web rails db:migrate
+docker compose run web rails db:seed
 docker compose run web rake release:create_songs
 docker compose run web rake release:update_covers
 docker compose run web rake songs:check_empty_urls
--- devops/rails/update.sh
+++ devops/rails/update.sh
@@ -3,11 +3,11 @@
 chmod +x ./devops/rails/update.sh
 
 docker compose run web bundle install
-docker compose run web rails db:environment:set RAILS_ENV=development
-docker compose run web rails db:migrate RAILS_ENV=development
-docker compose run web rails db:seed RAILS_ENV=development
-docker compose run web rake release:update_covers RAILS_ENV=development
-docker compose run web rake songs:check_empty_urls RAILS_ENV=development
+docker compose run web rails db:environment:set
+docker compose run web rails db:migrate
+docker compose run web rails db:seed
+docker compose run web rake release:update_covers
+docker compose run web rake songs:check_empty_urls
 
 docker compose run web rspec   
 docker container prune -f
