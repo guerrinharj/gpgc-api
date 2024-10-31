059e46295a37c12418c035954083ee3259e0ddd2
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-31 17:31:01 -0300

    changed env and environments

--- devops/rails/restart.sh
+++ devops/rails/restart.sh
@@ -5,11 +5,11 @@ bundle install
 docker compose run web bundle install
 
 docker compose run web rails db:environment:set
-docker compose run web rails db:drop
-docker compose run web rails db:create
+docker compose run web rails db:drop ENV=development
+docker compose run web rails db:create ENV=development
 docker compose run web rails active_storage:install
-docker compose run web rails db:migrate
-docker compose run web rails db:seed
+docker compose run web rails db:migrate ENV=development
+docker compose run web rails db:seed ENV=development
 docker compose run web rake release:create_songs
 docker compose run web rake release:update_covers
 docker compose run web rake songs:check_empty_urls
