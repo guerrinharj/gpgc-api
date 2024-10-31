d389adefe62f794b5813ba055b3e03ffa6722b24
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-31 18:22:01 -0300

    removed warnings

--- devops/compose/exec.sh
+++ devops/compose/exec.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env ruby
 
 #set -e
 
@@ -8,6 +8,9 @@
 #ARG_USER_UID=$ARG_USER_UID \
 #ARG_USER_GID=$ARG_USER_GID \
 #DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM \
+export RAILS_ENV=production
+
+
 chmod +x ./devops/compose/exec.sh
 docker compose exec web bash $@
 docker compose exec web bash bundle install
