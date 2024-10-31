2baaf86c6efcdeeba30329bea70c905f246cc97c
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-31 18:18:57 -0300

    fixed warnings in develop

--- devops/compose/exec.sh
+++ devops/compose/exec.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/local/bin/ruby
 
 #set -e
 
@@ -8,6 +8,8 @@
 #ARG_USER_UID=$ARG_USER_UID \
 #ARG_USER_GID=$ARG_USER_GID \
 #DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM \
+export RAILS_ENV=development
+
 chmod +x ./devops/compose/exec.sh
 docker compose exec web bash $@
 docker compose exec web bash bundle install
