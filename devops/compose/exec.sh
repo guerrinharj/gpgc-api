a898b5c4bb3b15ec9b9bda6bf7c6512a6289e67b
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-10-31 19:18:07 -0300

    shebag back

--- devops/compose/exec.sh
+++ devops/compose/exec.sh
@@ -8,7 +8,6 @@
 #ARG_USER_UID=$ARG_USER_UID \
 #ARG_USER_GID=$ARG_USER_GID \
 #DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM \
-export RAILS_ENV=development
 
 chmod +x ./devops/compose/exec.sh
 docker compose exec web bash $@
