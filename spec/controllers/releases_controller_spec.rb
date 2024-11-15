c019a54a07c06544d1ff8c150c8e451ee188ef93
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2024-11-15 19:39:15 -0300

    did create and other http_status

--- spec/controllers/releases_controller_spec.rb
+++ spec/controllers/releases_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::ReleasesController, type: :controller do
     let(:user) { create(:user) }
     let(:artist) { create(:artist, user: user) }
+    let(:release) { create(:release, artist: artist, user: user) }
 
     before do
         request.headers['Username'] = user.username
@@ -12,9 +13,26 @@ RSpec.describe Api::V1::ReleasesController, type: :controller do
     end
 
     describe 'POST #create' do
+        it 'returns success if release is created' do
+            post :create, params: { release: attributes_for(:release).merge(artist_id: artist.id) }
+            expect(response).to have_http_status(:created)
+        end
+
         it 'returns unprocessable_entity status when parameters are invalid' do
             post :create, params: { release: { name: nil, artist_id: nil } }
             expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'returns unauthorized status if given wrong password' do
+            request.headers['Password'] = 'wrongpassword'
+            post :create, params: { release: attributes_for(:release) }
+            expect(response).to have_http_status(:unauthorized)
+        end
+
+        it 'returns forbidden status if user is not current_user' do
+            allow(controller).to receive(:current_user).and_call_original
+            post :create, params: { release: attributes_for(:release) }
+            expect(response).to have_http_status(:forbidden)
         end
     end
 end
